5d07e3fc2ffd72b1d6d65edc95f24ab010b9bf7a
Gabriele <gabrieletramontini.aluno@unipampa.edu.br>
2026-05-15 18:25:14 -0300

    versao_2

--- bib.h
+++ bib.h
@@ -4,87 +4,91 @@
 #define MAX_MEM 256
 #define MAX_REG 8
 #define MEM 128
+#define INST_END 128
 
 
-typedef enum { 
-    tipo_R, tipo_J, tipo_I, tipo_dado, tipo_outros 
-} classe_inst;
+typedef enum { tipo_R, tipo_J, tipo_I, tipo_dado, tipo_outros } classe_inst;
 
 typedef enum {
-    S_FETCH=0, S_DECODE=1, S_MEM_ADDR=2, S_MEM_READ=3, S_MEM_WB=4,
-    S_MEM_WRITE=5, S_ADDI_WB=6, S_R_EXEC=7, S_R_WB=8, S_BEQ=9, S_JUMP=10
-} estado;
+  S_FETCH = 0,
+  S_DECODE = 1,
+  S_MEM_ADDR = 2,
+  S_MEM_READ = 3,
+  S_MEM_WB = 4,
+  S_MEM_WRITE = 5,
+  S_ADDI_WB = 6,
+  S_R_EXEC = 7,
+  S_R_WB = 8,
+  S_BEQ = 9,
+  S_JUMP = 10
+} Estado;
 
-
+//STRUCTs
+typedef struct {
+  classe_inst tipo;
+  char bin[17];
+  int opcode, rs, rt, rd, funct, imm, addr;
+  int dado;
+} Mem_in;
 
 typedef struct {
-    classe_inst tipo;
-    char bin[17];
-    int opcode, rs, rt, rd, funct, imm, addr;
-    int dado;
-} memoria;
+  Mem_in IR;
+  int MDR, A, B, ULASaida;
+} Regs_inter;
 
 typedef struct {
-    memoria IR;
-    int MDR, A, B, ULASaida;
-} regs_inter;
+  int PCEsc, IouD, EscMem, IREsc, MemParaReg, EscReg;
+  int ULAFonteA, ULAFonteB, ControleULA, PCFonte, RegDst, Branch;
+} Sinais;
 
 typedef struct {
-    int PCEsc, IouD, EscMem, IREsc, MemParaReg, EscReg;
-    int ULAFonteA, ULAFonteB, ControleULA, PCFonte, RegDst, Branch;
-    int le_mem;
-} sinais;
+  int total_inst, add, sub, and_op, or_op;
+  int addi, sw, lw, beq, jump;
+  int total_r, total_i, total_j;
+  int ciclos_clock;
+} Estatisticas;
 
 typedef struct {
-    int total_inst, add, sub, and_op, or_op;
-    int addi, sw, lw, beq, jump;
-    int total_r, total_i, total_j;
-    int ciclos_clock;
-} estatisticas;
+  int pc, estado, instrucoes_exec;
+  int dados[MAX_MEM];
+  char reg[MAX_REG];
+  Regs_inter inter;
+  Estatisticas est;
+} Salva_CPU;
 
 typedef struct {
-    int pc, estado, instrucoes_exec;
-    char reg[MAX_REG];
-    int dados[MEM];
-    regs_inter inter;
-    estatisticas est;
-} salva_cpu;
-
-typedef struct {
-    int pc, estado_atual, ciclos_clock, instrucoes_exec, i_hist;
-    memoria memoria[MAX_MEM];
-    int num_instrucoes;
-    char reg[MAX_REG];
-    char *NOME_REG[MAX_REG];
-    regs_inter inter;
-    estatisticas est;
-    salva_cpu historico[MAX_MEM];
+  int pc, estado_atual, num_instrucoes, ciclos_clock, instrucoes_exec, i_hist;
+  char reg[MAX_REG], *NOME_REG[MAX_REG];
+  Mem_in memoria[MAX_MEM];
+  Regs_inter inter;
+  Estatisticas est;
+  Salva_CPU historico[MAX_MEM];
 } CPU;
 
-
+//PROTOTIPOS
 void limpa_buffer();
 void carrega_mem(CPU *cpu);
 void inicializa_cpu(CPU *cpu);
 
-int separa_bits(char *b, int ini, int nBits);
-int bits_imm(char *b, int ini, int nBits);
+int separa_bits(char *b, int ini, int n);
+int bits_imm(char *b, int ini, int n);
 int bits_jump(char *b);
 int bin_to_int16(char *b);
-void decoder(memoria *mem);
-sinais gera_sinais(int estado, int funct); 
+void decode(Mem_in *e);
+Sinais gera_sinais(int estado, int funct);
 
 int ula(int A, int B, int ctrl, int *ovf, int *zero);
-void salvar_estado(CPU *cpu); 
+void salvar_estado(CPU *cpu);
 int proximo_estado(int estado, int opcode);
 void executa_ciclo(CPU *cpu);
 void executa_instrucao(CPU *cpu);
 void executa_programa(CPU *cpu);
-void atualiza_estatisticas(CPU *cpu);
+void atualiza_Estatisticas(CPU *cpu);
 void volta_ciclo(CPU *cpu);
 void volta_instrucao(CPU *cpu);
 void reset_cpu(CPU *cpu);
 
-void disassembla(memoria *mem, char *buf);
+void disassembla(Mem_in *e, char *buf);
 void print_mem(CPU *cpu);
 void print_regs(CPU *cpu);
 void print_inter(CPU *cpu);
--- main.c
+++ main.c
@@ -3,81 +3,93 @@
 #include <string.h>
 #include "bib.h"
 
-int main(){
-    CPU cpu;
-    inicializa_cpu(&cpu);
-    int menu = -1;
-    
-    do {
-        printf("\n-------------------  MINI MIPS 8 BITS MULTICICLO  -------------------\n"
-            "1. Carregar memoria (.mem)\n"
-            "2. Executar programa (Run)\n"
-            "3. Executar uma instrucao completa (Step Instrucao)\n"
-            "4. Executar um ciclo de clock (Step Ciclo)\n"
-            "5. Voltar um ciclo (Back Step)\n"
-            "6. Voltar instrucao\n"
-            "7. Resetar programa\n"
-            "8. Imprimir memoria\n"
-            "9. Imprimir banco de registradores\n"
-            "10. Imprimir registradores intermediarios\n"
-            "11. Imprimir todo o simulador\n"
-            "12. Imprimir estatisticas\n"
-            "13. Salvar .asm\n"
-            "14. Salvar .dat\n"
-            "0. Sair\n");
-        printf("Escolha uma opcao: ");
-        scanf("%d", &menu);
-
-        switch(menu){
-            case 1: 
-                carrega_mem(&cpu); 
-            break;
-            case 2: 
-                executa_programa(&cpu); 
-            break;
-            case 3: 
-                executa_instrucao(&cpu); 
-            break;
-            case 4:
-                executa_ciclo(&cpu); 
-            break;
-            case 5: 
-                volta_ciclo(&cpu); 
-            break;
-            case 6: 
-                volta_instrucao(&cpu); 
-            break;
-            case 7: 
-                reset_cpu(&cpu); 
-            break;
-            case 8: 
-                print_mem(&cpu); 
-            break;
-            case 9: 
-                print_regs(&cpu); 
-            break;
-            case 10: 
-                print_inter(&cpu); 
-            break;
-            case 11: 
-                print_complete(&cpu); 
-            break;
-            case 12: 
-                print_est(&cpu); 
-            break;
-            case 13: 
-                salva_asm(&cpu); 
-            break;
-            case 14: 
-                salva_dat(&cpu); 
-            break;
-            case 0: 
-                printf("Encerrando.\n"); 
-            break;
-            default: 
-                printf("Opcao invalida!\n"); 
-            break;
-        }
-    } while(menu!=0);
-    return 0;
+int main() {
+  CPU cpu;
+  inicializa_cpu(&cpu);
+  int menu;
+  do {
+    printf("\n-------------------  MINI MIPS 8 BITS MULTICICLO  "
+           "-------------------\n"
+           " 1. Carregar memoria (.mem)\n"
+           " 2. Executar programa (Run)\n"
+           " 3. Executar uma instrucao completa (Step Instrucao)\n"
+           " 4. Executar um ciclo de clock (Step Ciclo)\n"
+           " 5. Voltar um ciclo (Back Step)\n"
+           " 6. Resetar programa\n"
+           " 7. Imprimir memoria\n"
+           " 8. Imprimir banco de registradores\n"
+           " 9. Imprimir registradores intermediarios\n"
+           "10. Imprimir todo o simulador\n"
+           "11. Imprimir Estatisticas\n"
+           "12. Salvar .asm\n"
+           "13. Salvar .dat\n"
+           " 0. Sair\n"
+           "Opcao: ");
+    if (scanf("%d", &menu) != 1) {
+      printf("Opcao invalida.\n");
+      while (getchar() != '\n')
+        ;
+      menu = -1;
+      continue;
+    }
+    switch (menu) {
+    case 1:
+      carrega_mem(&cpu);
+      break;
+    case 2:
+      executa_programa(&cpu);
+      break;
+    case 3:
+      executa_instrucao(&cpu);
+      break;
+    case 4:
+      if (cpu.num_instrucoes == 0) {
+        printf("Nenhuma instrucao carregada.\n");
+        break;
+      }
+      if (cpu.estado_atual == 0 && cpu.memoria[cpu.pc].opcode == 5) {
+        printf("Instrucao de parada no PC %d.\n", cpu.pc);
+        break;
+      }
+      executa_ciclo(&cpu);
+      break;
+    case 5:
+      volta_ciclo(&cpu);
+      break;
+    case 6:
+      volta_instrucao(&cpu);
+      break;
+    case 7:
+      reset_cpu(&cpu);
+      break;
+    case 8:
+      print_mem(&cpu);
+      break;
+    case 9:
+      print_regs(&cpu);
+      break;
+    case 10:
+      print_inter(&cpu);
+      break;
+    case 11:
+      print_complete(&cpu);
+      break;
+    case 12:
+      print_est(&cpu);
+      break;
+    case 13:
+      salva_asm(&cpu);
+      break;
+    case 14:
+      salva_dat(&cpu);
+      break;
+    case 0:
+      printf("Encerrando.\n");
+      break;
+    default:
+      printf("Opcao invalida!\n");
+      break;
+    }
+  } while (menu != 0);
+  return 0;
 }
